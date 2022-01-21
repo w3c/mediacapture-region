f3038d3de911acaaaa54bf25ca8edff39b0e142d
Dominique Hazael-Massieux <dom@w3.org>
2022-01-21 14:26:55 +0100

    Remove latest version link
    Not available until FPWD
    fixes #7

--- region-capture.js
+++ region-capture.js
@@ -14,6 +14,6 @@ var respecConfig = {
     },
   ],
   xref: ["html", "infra", "permissions", "permissions-policy", "dom", "mediacapture-streams", "webidl", "screen-capture"],
-  shortName: "region-capture",
+  latestVersion: null,
   subjectPrefix: "[region-capture]",
 };
