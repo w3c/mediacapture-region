8020299123b9ebf247e471fb1a10cae2bbe6b9bc
Elad Alon <elad.alon@gmail.com>
2022-03-31 18:33:42 +0200

    s/ED/fpwd

--- region-capture.js
+++ region-capture.js
@@ -1,6 +1,6 @@
 var respecConfig = {
   group: "webrtc",
-  specStatus: "ED",
+  specStatus: "FPWD",
   github: {
     repoURL: "https://github.com/w3c/mediacapture-region/",
     branch: "main",
@@ -10,7 +10,7 @@ var respecConfig = {
       name: "Elad Alon",
       email: "eladalon@google.com",
       company: "Google",
-      w3cId:  118124
+      w3cid:  118124
     },
   ],
   xref: ["html", "infra", "permissions", "permissions-policy", "dom", "mediacapture-streams", "webidl", "screen-capture"],
